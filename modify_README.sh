d631a2e9ba9cff0d0c49103f0bba255c56bb9cea
rwoo <wooheaven79@gmail.com>
2017-12-05 01:08:46 +0900

    #68-replace-folder-with-readme-md renamed:    01_Ubuntu/02_16/04_bashShell_script/01_if_condition_1st_argument_existence_check.md -> 01_Ubuntu/02_16/02_bashShell_script/01_if_condition_1st_argument_existence_check.md renamed:    01_Ubuntu/02_16/04_bashShell_script/02_if_condition_count_of_arguments_check.md -> 01_Ubuntu/02_16/02_bashShell_script/02_if_condition_count_of_arguments_check.md renamed:    01_Ubuntu/02_16/04_bashShell_script/03_check_current_shell.md -> 01_Ubuntu/02_16/02_bashShell_script/03_check_current_shell.md renamed:    01_Ubuntu/02_16/19_date/01_date_with_hour_min_sec_nano.md -> 01_Ubuntu/02_16/04_date/01_date_with_hour_min_sec_nano.md renamed:    01_Ubuntu/02_16/06_df/01_df_with_human_readerble.md -> 01_Ubuntu/02_16/05_df/01_df_with_human_readerble.md renamed:    01_Ubuntu/02_16/03_du/01_du_file_size_of_folder.md -> 01_Ubuntu/02_16/06_du/01_du_file_size_of_folder.md renamed:    01_Ubuntu/02_16/20_exfat/01_install_exfat_on_ubuntu16.md -> 01_Ubuntu/02_16/07_exfat/01_install_exfat_on_ubuntu16.md renamed:    01_Ubuntu/02_16/22_firefox/01_firefox_downgrade_57_to_45.md -> 01_Ubuntu/02_16/09_firefox/01_firefox_downgrade_57_to_45.md renamed:    01_Ubuntu/02_16/09_for_statement/01_for_num++.md -> 01_Ubuntu/02_16/10_for_statement/01_for_num++.md renamed:    01_Ubuntu/02_16/09_for_statement/02_for_in_list.md -> 01_Ubuntu/02_16/10_for_statement/02_for_in_list.md renamed:    01_Ubuntu/02_16/09_for_statement/03_for_read_file.md -> 01_Ubuntu/02_16/10_for_statement/03_for_read_file.md renamed:    01_Ubuntu/02_16/10_if/01_if_check_filePath.md -> 01_Ubuntu/02_16/11_if/01_if_check_filePath.md renamed:    01_Ubuntu/02_16/11_LVM/01_LVM_on_attached_device.md -> 01_Ubuntu/02_16/12_LVM/01_LVM_on_attached_device.md renamed:    01_Ubuntu/02_16/12_mkfs.ext4/01_mkfs.ext4_device.md -> 01_Ubuntu/02_16/13_mkfs.ext4/01_mkfs.ext4_device.md renamed:    01_Ubuntu/02_16/13_mkfs.xfs/01_mkfs.xfs_device.md -> 01_Ubuntu/02_16/14_mkfs.xfs/01_mkfs.xfs_device.md renamed:    01_Ubuntu/02_16/14_nmap/01_install_use_nmap.md -> 01_Ubuntu/02_16/15_nmap/01_install_use_nmap.md renamed:    01_Ubuntu/02_16/15_rsync/01_rsync_folder_to_folder.md -> 01_Ubuntu/02_16/16_rsync/01_rsync_folder_to_folder.md renamed:    01_Ubuntu/02_16/16_sed/01_remove_^M_with_sed.md -> 01_Ubuntu/02_16/17_sed/01_remove_^M_with_sed.md renamed:    01_Ubuntu/02_16/16_sed/02_replace_every_3rd_comma_to_semicolon.md -> 01_Ubuntu/02_16/17_sed/02_replace_every_3rd_comma_to_semicolon.md renamed:    01_Ubuntu/02_16/17_substring/01_substring_remove.md -> 01_Ubuntu/02_16/18_substring/01_substring_remove.md renamed:    01_Ubuntu/02_16/04_tac/01_tac.md -> 01_Ubuntu/02_16/19_tac/01_tac.md renamed:    01_Ubuntu/02_16/05_vim/01_vimrc_encoding_korean.md -> 01_Ubuntu/02_16/20_vim/01_vimrc_encoding_korean.md renamed:    01_Ubuntu/02_16/05_vim/02_vimrc_append_save_macro.md -> 01_Ubuntu/02_16/20_vim/02_vimrc_append_save_macro.md renamed:    01_Ubuntu/02_16/05_vim/03_vim_diff.md -> 01_Ubuntu/02_16/20_vim/03_vim_diff.md renamed:    01_Ubuntu/02_16/06_window/01_resize_window.md -> 01_Ubuntu/02_16/22_window/01_resize_window.md renamed:    01_Ubuntu/02_16/22_zip/01_zip_directory.md -> 01_Ubuntu/02_16/23_zip/01_zip_directory.md modified:   README.md modified:   change_A_to_B.txt modified:   modify_README.sh

--- modify_README.sh
+++ modify_README.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# save values to variables from a file
-oldStr=$(awk 'BEGIN{FS=OFS=" "} {print $1}' change_A_to_B.txt)
-newStr=$(awk 'BEGIN{FS=OFS=" "} {print $3}' change_A_to_B.txt)
+# for with file
+while read line
+do
+	echo "line as String = "$line
+	echo ""
 
-# check variables
-echo "oldStr = "$oldStr
-echo "newStr = "$newStr
+	# save values to variables from a file
+	oldStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $1}'`
+	newStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $3}'`
+	
+	# check variables
+	echo "oldStr = "$oldStr
+	echo "newStr = "$newStr
+	
+	# grep $oldStr
+	grep $oldStr README.md
+	grep $oldStr README.md | sed -e "s|$oldStr|$newStr|"
+	echo ""
 
-# modify
-#grep $oldStr README.md | sed -e "s|$oldStr|$newStr|"
-sed -i "s|$oldStr|$newStr|" README.md
+	# modify
+	sed -i "s|$oldStr|$newStr|" README.md
+	
+	# final
+	unset oldStr
+	unset newStr
 
-# final
-unset oldStr
-unset newStr
+done < change_A_to_B.txt
