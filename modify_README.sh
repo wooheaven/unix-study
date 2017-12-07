43a40ef69e674013b8d2a5bd900fa788929ade67
rwoo <rwoo@bistel.com>
2017-12-07 18:44:54 +0900

    #69-modify-mac-zip renamed:    03_macOS/03_Battery/01_Charge_Status.md -> 03_macOS/01_Battery/01_Charge_Status.md renamed:    03_macOS/06_find/01_find_with_basename.md -> 03_macOS/02_find/01_find_with_basename.md renamed:    03_macOS/06_find/02_find_with_maxdepth.md -> 03_macOS/02_find/02_find_with_maxdepth.md renamed:    03_macOS/06_find/03_find_and_sed_move_CRLF_to_LF.md -> 03_macOS/02_find/03_find_and_sed_move_CRLF_to_LF.md renamed:    03_macOS/06_find/04_find_and_sed_move_TAB_to_4SPACES.md -> 03_macOS/02_find/04_find_and_sed_move_TAB_to_4SPACES.md renamed:    03_macOS/02_IntelliJ/01_Shortcuts.md -> 03_macOS/04_IntelliJ/01_Shortcuts.md deleted:    03_macOS/04_zip/01_use_zip.md renamed:    03_macOS/07_Python/01_with_Conda/01_Matplotlib_backends_RuntimeError.md -> 03_macOS/05_Python/01_with_Conda/01_Matplotlib_backends_RuntimeError.md renamed:    03_macOS/05_sed/01_sed_remove_CRLF_to_LF.md -> 03_macOS/06_sed/01_sed_remove_CRLF_to_LF.md renamed:    03_macOS/01_vim/01_vimrc_configure.md -> 03_macOS/08_vim/01_vimrc_configure.md renamed:    03_macOS/01_vim/02_vim_diff.md -> 03_macOS/08_vim/02_vim_diff.md renamed:    03_macOS/01_vim/03_vim_diff_refresh.md -> 03_macOS/08_vim/03_vim_diff_refresh.md renamed:    03_macOS/08_vnc_client/01_use_vnc_client.md -> 03_macOS/09_vnc_client/01_use_vnc_client.md new file:   03_macOS/10_zip/01_use_zip.md renamed:    03_macOS/09_zsh/01_explain_of_bash_profile_bashrc_zshrc.md -> 03_macOS/11_zsh/01_explain_of_bash_profile_bashrc_zshrc.md renamed:    03_macOS/09_zsh/02_example_of_zshrc.md -> 03_macOS/11_zsh/02_example_of_zshrc.md modified:   README.md modified:   change_A_to_B.txt modified:   modify_README.sh

--- modify_README.sh
+++ modify_README.sh
@@ -3,27 +3,28 @@
 # for with file
 while read line
 do
-	echo "line as String = "$line
-	echo ""
+	# echo
+echo "line as String = "$line
+echo ""
 
 	# save values to variables from a file
-	oldStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $1}'`
-	newStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $3}'`
+oldStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $1}'`
+newStr=`echo $line | awk 'BEGIN{FS=OFS=" "} {print $3}'`
 	
 	# check variables
-	echo "oldStr = "$oldStr
-	echo "newStr = "$newStr
+echo "oldStr = "$oldStr
+echo "newStr = "$newStr
 	
 	# grep $oldStr
-	grep $oldStr README.md
-	grep $oldStr README.md | sed -e "s|$oldStr|$newStr|"
-	echo ""
+grep $oldStr README.md
+grep $oldStr README.md | sed -e "s|$oldStr|$newStr|"
+echo ""
 
 	# modify
-	sed -i "s|$oldStr|$newStr|" README.md
+sed -i'' -e "s#${oldStr}#${newStr}#" README.md
 	
 	# final
-	unset oldStr
-	unset newStr
+unset oldStr
+unset newStr
 
 done < change_A_to_B.txt
